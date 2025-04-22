eb1a4a11ef988e33dfcfde3a206dd264eba56339
Muhammedrafikk <muhammedrafi6622@gmail.com>
2025-04-22 15:33:12 +0530

    update

--- src/app/components/Navbar/NavbaMain.tsx
+++ src/app/components/Navbar/NavbaMain.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function NavbaMain() {
   return (
     <div>
-      <h2 className="bg-blue-700 text-white text-3xl text-center">
-        Lorem ipsum, dolor sit amet consectetur adipisicing
+      <h2 className="bg-[#1b7005] text-white text-3xl text-center">
+        Welcome to the typing game!
       </h2>
     </div>
   );
