793a9fec6bd81335c66e4621a7a444f470213430
Muhammedrafikk <muhammedrafi6622@gmail.com>
2025-04-22 15:26:57 +0530

    update

--- src/app/components/Navbar/NavbaMain.tsx
+++ src/app/components/Navbar/NavbaMain.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function NavbaMain() {
   return (
-    <div>NavbaMain</div>
-  )
+    <div>
+      <h2 className="bg-blue-700 text-white text-3xl text-center">
+        Lorem ipsum, dolor sit amet consectetur adipisicing
+      </h2>
+    </div>
+  );
 }
